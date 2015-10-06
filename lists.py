fbcf335b3b13d89e158fb34955f48e4ae84c0764
Memo Garcia <g.backhoff@hotmail.com>
2015-10-06 15:58:40 -0500

    Fixed List, all options work now

--- lists.py
+++ lists.py
@@ -1,85 +1,70 @@
 
 keylist = {
-		"1":'botGamesPlayed',		
-		"2":'killingSpree',
-		"3":'maxChampionsKilled',#
-		"4":'maxLargestCriticalStrike',#
-		"5":'maxLargestKillingSpree',#
-		"6":'maxNumDeaths',#
-		"7":'maxTimePlayed',#
-		"8":'maxTimeSpentLiving',#
-		"9":'mostChampionKillsPerSession',#
-		"10":'mostSpellsCast',#
-		"11":'normalGamesPlayed',#
-		"12":'rankedPremadeGamesPlayed',#
-		"13":'rankedSoloGamesPlayed',#
-		"14":'totalAssists',#
-		"15":'totalChampionKills',
-		"16":'totalDamageDealt',
-		"17":'totalDamageTaken',
-		"18":'totalDeathsPerSession',
-		"19":'totalDoubleKills',
-		"20":'totalFirstBlood',
-		"21":'totalGoldEarned',
-		"22":'totalHeal',
-		"23":'totalMagicDamageDealt',
-		"24":'totalMinionKills',#
-		"25":'totalNeutralMinionsKilled',
+		"1":'maxChampionsKilled',	
+		"2":'maxNumDeaths',
+		"3":'mostChampionKillsPerSession',
+		"4":'mostSpellsCast',
+		"5":'totalAssists',
+		"6":'totalChampionKills',
+		"7":'totalDamageDealt',
+		"8":'totalDamageTaken',
+		"9":'totalDeathsPerSession',
+		"10":'totalDoubleKills',
+		"11":'totalTripleKills',
+		"12":'totalQuadraKills',
+		"13":'totalPentaKills',
+		"14":'totalFirstBlood',
+		"15":'totalGoldEarned',
+		"16":'totalMagicDamageDealt',
+		"17":'totalMinionKills',
+		"18":'totalTurretsKilled',
+		"19":'totalSessionsLost',
+		"20":'totalSessionsWon',
 	}
 
 namelist = {
-		"1":'Bot Games Played',		
-		"2":'Longest Killing Spree',
-		"3":'Total Champions Killed',
-		"4":'Largest Critical Strike',
-		"5":'Largest Killing Spree',
-		"6":'Max Number of Deaths',
-		"7":'Longest Time Playing',
-		"8":'Longest Time Alive',
-		"9":'Most Champions Killed per Session',
-		"10":'Most Spells Casted',
-		"11":'Normals Played',
-		"12":'Ranked Premades Played',
-		"13":'Ranked Solos Played',
-		"14":'Total Assists',
-		"15":'Total Champion Kills',
-		"16":'Total Damage Dealt',
-		"17":'Total Damage Taken',
-		"18":'Total Deaths per Session',
-		"19":'Total Double Kills',
-		"20":'Total First Bloods',
-		"21":'Total Gold  earned',
-		"22":'Total Heals',
-		"23":'Total Magic Damage Dealt',
-		"24":'Total Minion Kills',
-		"25":'Total Neutral Minion Killed',
+		"1":'Max Champions Killed',		
+		"2":'Max Number of Deaths',
+		"3":'Most Champions Killed in a Session',
+		"4":'Most Spells Casted',
+		"5":'Total Assists',
+		"6":'Total Champions Killed',
+		"7":'Total Damage Dealt',
+		"8":'Total Damage Taken',
+		"9":'Total Deaths Per Session',
+		"10":'Total Double Kills',
+		"11":'Total Triple Kills',
+		"12":'Total Quadra Kills',
+		"13":'Total Penta Kills',
+		"14":'Total First Bloods',
+		"15":'Total Gold Earned',
+		"16":'Total Magic Damage Dealt',
+		"17":'Total Minion Kills',
+		"18":'Total Turrets Killed',
+		"19":'Total Games Lost',
+		"20":'Total Games Won',
 }
 
 menu = """
-	1. Bot Games Played	
-	2. Killing Spree
-	3. Max Champion Kills per Champion
-	4. Largest Critical Strike
-	5. Largest Killing Spree	
-	6. Max Number of Deaths
-	7. Max Time Played
-	8. Max Time Alive
-	9. Most Champion Kills per Session
-	10. Most Spells Casted
-	11. Normals Played
-	12. Ranked Premades Played
-	13. Ranked Solos Played
-	14. Total Assists
-	15. Total Champion Kills
-	16. Total Damage Dealt	
-	17. Total Damage Taken
-	18. Total Deaths per Session
-	19. Total Double Kills
-	20. Total First Bloods
-	21. Total Gold Earned
-	22. Total Healing
-	23. Total Magic Damage Dealt
-	24. Total Minion Kills
-	25. Total Neutral Minion Kills
+	1. Max Champions Killed
+	2. Max Number of Deaths
+	3. Most Champions Killed in a Session
+	4. Most Spells Casted
+	5. Total Assists	
+	6. Total Champions Killed
+	7. Total Damage Dealt
+	8. Total Damage Dealt
+	9. Total Deaths Per Session
+	10. Total Double Kills
+	11. Total Triple Kills
+	12. Total Quadra Kills
+	13. Total Penta Kills
+	14. Total Firsts Bloods
+	15. Total Gold Earned
+	16. Total Magic Damage Dealt	
+	17. Total Total Minion Kills
+	18. Total Turrets Killed
+	19. Total Sessions Lost
+	20. Total Session Won
 	"""
 
