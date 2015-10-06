a1229c8b2e2f3d1e0267ac9992f9f8a638fc7ad3
backhoff <g.backhoff@hotmail.com>
2015-10-05 19:59:09 -0500

    Added Features to main script. Added two files with dicts inside. Steamnews simple script to write files to a text file.

---
+++ lists.py
@@ -0,0 +1,85 @@
+
+keylist = {
+		"1":'botGamesPlayed',		
+		"2":'killingSpree',
+		"3":'maxChampionsKilled',#
+		"4":'maxLargestCriticalStrike',#
+		"5":'maxLargestKillingSpree',#
+		"6":'maxNumDeaths',#
+		"7":'maxTimePlayed',#
+		"8":'maxTimeSpentLiving',#
+		"9":'mostChampionKillsPerSession',#
+		"10":'mostSpellsCast',#
+		"11":'normalGamesPlayed',#
+		"12":'rankedPremadeGamesPlayed',#
+		"13":'rankedSoloGamesPlayed',#
+		"14":'totalAssists',#
+		"15":'totalChampionKills',
+		"16":'totalDamageDealt',
+		"17":'totalDamageTaken',
+		"18":'totalDeathsPerSession',
+		"19":'totalDoubleKills',
+		"20":'totalFirstBlood',
+		"21":'totalGoldEarned',
+		"22":'totalHeal',
+		"23":'totalMagicDamageDealt',
+		"24":'totalMinionKills',#
+		"25":'totalNeutralMinionsKilled',
+	}
+
+namelist = {
+		"1":'Bot Games Played',		
+		"2":'Longest Killing Spree',
+		"3":'Total Champions Killed',
+		"4":'Largest Critical Strike',
+		"5":'Largest Killing Spree',
+		"6":'Max Number of Deaths',
+		"7":'Longest Time Playing',
+		"8":'Longest Time Alive',
+		"9":'Most Champions Killed per Session',
+		"10":'Most Spells Casted',
+		"11":'Normals Played',
+		"12":'Ranked Premades Played',
+		"13":'Ranked Solos Played',
+		"14":'Total Assists',
+		"15":'Total Champion Kills',
+		"16":'Total Damage Dealt',
+		"17":'Total Damage Taken',
+		"18":'Total Deaths per Session',
+		"19":'Total Double Kills',
+		"20":'Total First Bloods',
+		"21":'Total Gold  earned',
+		"22":'Total Heals',
+		"23":'Total Magic Damage Dealt',
+		"24":'Total Minion Kills',
+		"25":'Total Neutral Minion Killed',
+}
+
+menu = """
+	1. Bot Games Played	
+	2. Killing Spree
+	3. Max Champion Kills per Champion
+	4. Largest Critical Strike
+	5. Largest Killing Spree	
+	6. Max Number of Deaths
+	7. Max Time Played
+	8. Max Time Alive
+	9. Most Champion Kills per Session
+	10. Most Spells Casted
+	11. Normals Played
+	12. Ranked Premades Played
+	13. Ranked Solos Played
+	14. Total Assists
+	15. Total Champion Kills
+	16. Total Damage Dealt	
+	17. Total Damage Taken
+	18. Total Deaths per Session
+	19. Total Double Kills
+	20. Total First Bloods
+	21. Total Gold Earned
+	22. Total Healing
+	23. Total Magic Damage Dealt
+	24. Total Minion Kills
+	25. Total Neutral Minion Kills
+	"""
+
--- riotapi.py
+++ riotapi.py
@@ -1,44 +1,73 @@
 import json
 import urllib2
+from lists import keylist
+from lists import namelist
+from lists import menu
 
+key = '9c810c33-25b4-4857-b974-5576d0eefc38'
+
+#Get summoner ID by name
 def get_summoner_id(summoner):
 	url_summoner = summoner.replace(' ', '%20')
 	data_summoner = summoner.replace(' ', '')
-	url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+url_summoner+"?api_key=da5ff0e5-d859-4d7b-a3a1-c8df657df6c9"
+	url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+url_summoner+"?api_key=" + key
 	json_obj = 	urllib2.urlopen(url)
 	data = json.load(json_obj)
 	summoner_id = data[data_summoner]['id']
 
 	return summoner_id
 
-def season_info(season,summoner_id):
+#Display Information about a certain ranked season
+def season_info(season,summoner_id, doThis):
 	if season == 'season3':
 		season = 'SEASON3'
 	elif season == 'season4':
 		season = 'SEASON2014'
-
-
-	url = "https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/"+str(summoner_id)+"/ranked?season="+season+"&api_key=da5ff0e5-d859-4d7b-a3a1-c8df657df6c9"
+	url = "https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/"+str(summoner_id)+"/ranked?season="+season+"&api_key=" + key
 	json_obj = urllib2.urlopen(url)
 	data = json.load(json_obj)
+	total = 0
 	
-	print data
-	#for name,value in data['champions'].items():
-		#for name,value in value['stats'].items():
-			#print "Total Minion Kills with champion Name = " + str(id_to_name(item['id'])) + ": " + str(item['stats']['totalMinionKills'])
-		#print item['stats']['totalQuadraKills']
 
+
+	for item in data['champions']:
+			champ_name = str(id_to_name(item['id']))
+			if str(champ_name) == '0':
+				champ_name = 'All'
+			print str(namelist[doThis]) + " by " + champ_name + ": " + str(item['stats'][keylist[doThis]]) 
+		
+
+
+
+#Change champion ID  to name
 def id_to_name(champ_id):
-	url = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/"+str(champ_id)+"?api_key=da5ff0e5-d859-4d7b-a3a1-c8df657df6c9"
-	json_obj = urllib2.urlopen(url)
-	data = json.load(json_obj)
-	champ_name = str(data['name'])
+	if champ_id != 0:
+		url = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/"+str(champ_id)+"?locale=en_US&api_key=" + key
+		json_obj = urllib2.urlopen(url)
+		data = json.load(json_obj)
+		champ_name = data['key']
 
-	return champ_name
+		return champ_name
+
+	else:
+		return 0
+
 
 
 
 summoner = raw_input("Enter summoner name: ")
-summoner_id = get_summoner_id(summoner)
 season = raw_input("Enter \"season3\" or \"season4\" to choose a season to display info about: ")
-season_info(season,summoner_id)
+
+print(menu)
+
+doThis = raw_input("Choose One: ")
+
+
+#file_name = raw_input("All your information will be dumped in a text file, pick a name for it: ")
+
+summoner_id = get_summoner_id(summoner.lower())
+season_info(season,summoner_id, doThis)
+
+
+
+
