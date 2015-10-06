cbd5a3abf67e720bc594cce09f5554ec03fa974e
Memo Garcia <g.backhoff@hotmail.com>
2015-10-06 14:46:12 -0500

    Fixed Bug and Added champion select feature

--- riotapi.py
+++ riotapi.py
@@ -17,7 +17,27 @@ def get_summoner_id(summoner):
 
 	return summoner_id
 
-#Display Information about a certain ranked season
+
+def season_info_single(season,summoner_id,doThis,champOrAll):
+	if season == 'season3':
+		season = 'SEASON3'
+	elif season == 'season4':
+		season = 'SEASON2014'
+	url = "https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/"+str(summoner_id)+"/ranked?season="+season+"&api_key=" + key
+	json_obj = urllib2.urlopen(url)
+	data = json.load(json_obj)
+	found = False
+	champion = champOrAll.replace(' ','')
+
+	for item in data['champions']:
+		if str(id_to_name(item['id'])).lower() == champion.lower():
+			print str(namelist[doThis]) + " by " + str(id_to_name(item['id'])) + ": " + str(item['stats'][keylist[doThis]]) 
+			found = True
+			break
+	if found == False:
+		print("You did not use " + champion.title() + " on " + season)
+
+#Display Information about a certain ranked season for all champions
 def season_info(season,summoner_id, doThis):
 	if season == 'season3':
 		season = 'SEASON3'
@@ -28,6 +48,7 @@ def season_info(season,summoner_id, doThis):
 	data = json.load(json_obj)
 	count = 1
 	total = 0
+
 
 	for item in data['champions']:
 			champ_name = str(id_to_name(item['id']))
@@ -41,9 +62,7 @@ def season_info(season,summoner_id, doThis):
 				total = item['stats'][keylist[doThis]]
 	print str(namelist[doThis]) + " by All: " + str(total)
 
-
 		
-
 
 
 #Change champion ID  to name
@@ -65,15 +84,20 @@ def id_to_name(champ_id):
 summoner = raw_input("Enter summoner name: ")
 season = raw_input("Enter \"season3\" or \"season4\" to choose a season to display info about: ")
 
-print(menu)
+champOrAll = raw_input('Enter the name of a Champion to find his stats or \'all\' to display stats of all champions: ')
 
+print(menu)
 doThis = raw_input("Choose One: ")
 
 
 #file_name = raw_input("All your information will be dumped in a text file, pick a name for it: ")
 
 summoner_id = get_summoner_id(summoner.lower())
-season_info(season,summoner_id, doThis)
+
+if champOrAll.lower() == 'all':
+	season_info(season,summoner_id, doThis)
+elif champOrAll != 'all':
+	season_info_single(season,summoner_id, doThis,champOrAll)
 
 
 
