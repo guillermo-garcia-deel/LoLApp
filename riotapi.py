e1b0c84e0207008caccd66aae71c06651092ad5f
Memo Garcia <g.backhoff@hotmail.com>
2015-10-06 17:49:23 -0500

    Added option to choose region and infinite looping until user types q

--- riotapi.py
+++ riotapi.py
@@ -7,10 +7,10 @@ from lists import menu
 key = '9c810c33-25b4-4857-b974-5576d0eefc38'
 
 #Get summoner ID by name
-def get_summoner_id(summoner):
+def get_summoner_id(summoner,region):
 	url_summoner = summoner.replace(' ', '%20')
 	data_summoner = summoner.replace(' ', '')
-	url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+url_summoner+"?api_key=" + key
+	url = "https://"+str(region)+".api.pvp.net/api/lol/"+str(region)+"/v1.4/summoner/by-name/"+url_summoner+"?api_key=" + key
 	json_obj = 	urllib2.urlopen(url)
 	data = json.load(json_obj)
 	summoner_id = data[data_summoner]['id']
@@ -18,12 +18,12 @@ def get_summoner_id(summoner):
 	return summoner_id
 
 
-def season_info_single(season,summoner_id,doThis,champOrAll):
+def season_info_single(season,summoner_id,doThis,champOrAll,region):
 	if season == 'season3':
 		season = 'SEASON3'
 	elif season == 'season4':
 		season = 'SEASON2014'
-	url = "https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/"+str(summoner_id)+"/ranked?season="+season+"&api_key=" + key
+	url = "https://"+str(region)+".api.pvp.net/api/lol/"+str(region)+"/v1.3/stats/by-summoner/"+str(summoner_id)+"/ranked?season="+season+"&api_key=" + key
 	json_obj = urllib2.urlopen(url)
 	data = json.load(json_obj)
 	found = False
@@ -38,12 +38,12 @@ def season_info_single(season,summoner_id,doThis,champOrAll):
 		print("You did not use " + champion.title() + " on " + season)
 
 #Display Information about a certain ranked season for all champions
-def season_info(season,summoner_id, doThis):
+def season_info(season,summoner_id, doThis,region):
 	if season == 'season3':
 		season = 'SEASON3'
 	elif season == 'season4':
 		season = 'SEASON2014'
-	url = "https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/"+str(summoner_id)+"/ranked?season="+season+"&api_key=" + key
+	url = "https://"+str(region)+".api.pvp.net/api/lol/"+str(region)+"/v1.3/stats/by-summoner/"+str(summoner_id)+"/ranked?season="+season+"&api_key=" + key
 	json_obj = urllib2.urlopen(url)
 	data = json.load(json_obj)
 	count = 1
@@ -80,24 +80,46 @@ def id_to_name(champ_id):
 
 
 
-
+running = True
+print('Type in \'q\' or \'Q\' to exit the program.\n\n')
 summoner = raw_input("Enter summoner name: ")
-season = raw_input("Enter \"season3\" or \"season4\" to choose a season to display info about: ")
 
-champOrAll = raw_input('Enter the name of a Champion to find his stats or \'all\' to display stats of all champions: ')
-
-print(menu)
-doThis = raw_input("Choose One: ")
+if summoner.lower() == 'q':
+	quit()
 
 
-#file_name = raw_input("All your information will be dumped in a text file, pick a name for it: ")
+region = raw_input('Enter region (na, eu, kr, br eune, euw, lan, las, oce, ru, tr): ')
 
-summoner_id = get_summoner_id(summoner.lower())
+if region.lower() == 'q':
+	quit()
 
-if champOrAll.lower() == 'all':
-	season_info(season,summoner_id, doThis)
-elif champOrAll != 'all':
-	season_info_single(season,summoner_id, doThis,champOrAll)
+while running == True:
+	season = raw_input("Enter \"season3\" or \"season4\" to choose a season to display info about: ")
+	if season.lower() == 'q':
+		running = False
+		break
+
+	champOrAll = raw_input('Enter the name of a Champion to find his stats or \'all\' to display stats of all champions: ')
+	if champOrAll.lower() == 'q':
+		runnig = False
+		break
+
+	print(menu)
+
+	doThis = raw_input("Choose One: ")
+	if doThis.lower() == 'q':
+		running = False
+		break
+
+
+	#file_name = raw_input("All your information will be dumped in a text file, pick a name for it: ")
+
+	summoner_id = get_summoner_id(summoner.lower(),region)
+
+	if champOrAll.lower() == 'all':
+		season_info(season,summoner_id, doThis, region)
+	elif champOrAll != 'all':
+		season_info_single(season,summoner_id, doThis,champOrAll, region)
 
 
 
