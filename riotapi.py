c87684020c4001eee2239f5141569b1b07d26f92
backhoff <g.backhoff@hotmail.com>
2015-10-14 04:34:34 +0000

    Added LED light show

--- riotapi.py
+++ riotapi.py
@@ -1,10 +1,47 @@
 import json
 import urllib2
+import time
+import signal
+import sys
+import random
+import RPi.GPIO as GPIO
 from lists import keylist
 from lists import namelist
 from lists import menu
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
+GPIO.setup(22,GPIO.OUT)
+
+
 key = '9c810c33-25b4-4857-b974-5576d0eefc38'
+
+#Function to do random light show
+def light_show():
+	for i in range(0,2):
+		leds = [17,22,27]
+		num = leds[random.randrange(0,3,1)]
+		GPIO.output(num,GPIO.HIGH)
+		time.sleep(0.1)
+		GPIO.output(num,GPIO.LOW)
+
+def light_show_end():
+	for i in range (0,10):
+		leds = [17,22,27]
+		for i in range (0,3):
+			GPIO.output(leds[i],GPIO.HIGH)
+			time.sleep(0.2)
+			GPIO.output(leds[i],GPIO.LOW)
+
+#Signal Handler to detect keyboard input to close program.
+def signal_handler(signal, frame):
+	GPIO.output(17,GPIO.LOW)
+	GPIO.output(22,GPIO.LOW)		
+	GPIO.output(27,GPIO.LOW)
+	sys.exit()
+
 
 #Get summoner ID by name
 def get_summoner_id(summoner,region):
@@ -32,10 +69,12 @@ def season_info_single(season,summoner_id,doThis,champOrAll,region):
 	for item in data['champions']:
 		if str(id_to_name(item['id'])).lower() == champion.lower():
 			print str(namelist[doThis]) + " by " + str(id_to_name(item['id'])) + ": " + str(item['stats'][keylist[doThis]]) 
+			light_show()
 			found = True
 			break
 	if found == False:
 		print("You did not use " + champion.title() + " on " + season)
+		light_show()
 
 #Display Information about a certain ranked season for all champions
 def season_info(season,summoner_id, doThis,region):
@@ -56,12 +95,13 @@ def season_info(season,summoner_id, doThis,region):
 				champ_name = 'All'
 
 			if champ_name != 'All':	
-				print str(count) + ". " + str(namelist[doThis]) + " by " + champ_name + ": " + str(item['stats'][keylist[doThis]]) 
+				print str(count) + ". " + str(namelist[doThis]) + " by " + champ_name + ": " + str(item['stats'][keylist[doThis]])
+				light_show()
 				count += 1
 			if champ_name == 'All':
 				total = item['stats'][keylist[doThis]]
 	print str(namelist[doThis]) + " by All: " + str(total)
-
+	light_show_end()
 		
 
 
@@ -79,7 +119,7 @@ def id_to_name(champ_id):
 		return 0
 
 
-
+signal.signal(signal.SIGINT, signal_handler)
 running = True
 print('Type in \'q\' or \'Q\' to exit the program.\n\n')
 summoner = raw_input("Enter summoner name: ")
